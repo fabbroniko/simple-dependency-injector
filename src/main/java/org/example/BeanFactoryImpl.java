4f5083a0a92bea7ff9e3d795b581c8113b0dd4bb
Nicola Fabbrini <fabbrini.nicola@gmail.com>
2024-08-28 21:08:37 +1000

    Recursively initializing components and its dependencies. Circular not handled correctly yet

--- src/main/java/org/example/BeanFactoryImpl.java
+++ src/main/java/org/example/BeanFactoryImpl.java
@@ -1,32 +1,35 @@
 package org.example;
 
-import org.example.exception.MissingDependencyException;
+import org.example.exception.DependencyNotInitializableException;
 import org.example.registry.Registry;
 
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 public class BeanFactoryImpl implements BeanFactory {
 
     @Override
-    public Optional<Object> create(final Registry registry, final Class<?> clazz) {
-        final Constructor<?> constructor = clazz.getConstructors()[0];
-        final Class<?>[] params = constructor.getParameterTypes();
-        final List<Object> vals;
-        try {
-            vals = Arrays.stream(params)
-                .map(m -> registry.getInstance(m).orElseThrow(MissingDependencyException::new))
-                .toList();
-        } catch (MissingDependencyException e) {
-            return Optional.empty();
+    public Object create(final Registry registry, final Set<Class<?>> definedControllers, final Class<?> clazz) {
+        Class<?> targetClass = clazz;
+        if (clazz.isInterface()) {
+            targetClass = definedControllers.stream()
+                .filter(clazz::isAssignableFrom)
+                .findAny()
+                .orElseThrow(DependencyNotInitializableException::new);
         }
+
+        final Constructor<?> constructor = targetClass.getConstructors()[0];
+        final Class<?>[] params = constructor.getParameterTypes();
+        final List<Object> vals = Arrays.stream(params)
+                .map(m -> registry.getInstance(m).orElseGet(() -> create(registry, definedControllers, m)))
+                .toList();
 
         try {
             final Object instance = constructor.newInstance(vals.toArray());
             registry.insert(clazz, instance);
-            return Optional.of(instance);
+            return instance;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/org/example/DependencyInjector.java
+++ src/main/java/org/example/DependencyInjector.java
@@ -2,19 +2,17 @@ package org.example;
 
 import org.example.annotation.Component;
 import org.example.annotation.Configuration;
-import org.example.exception.CircularDependencyException;
 import org.example.registry.Registry;
 import org.example.registry.SimpleRegistryFactory;
 import org.example.scan.AnnotationPresentPredicate;
 import org.example.scan.AnnotationScanner;
 import org.example.scan.ClassScanner;
 import org.example.scan.ClasspathClassScanner;
-import org.example.scan.GenericAnnotationScanner;
 import org.example.scan.ContentFactoryImpl;
+import org.example.scan.GenericAnnotationScanner;
 import org.example.scan.SystemClassLoaderResourceLocator;
 import org.example.scan.URIFileFactory;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class DependencyInjector {
@@ -30,18 +28,11 @@ public class DependencyInjector {
 
         registry = new SimpleRegistryFactory().create();
 
-        do {
-            final Set<Class<?>> processedClasses = new HashSet<>();
-            for (Class<?> clazz : annotatedClasses) {
-                beanFactory.create(registry, clazz).ifPresent(instance -> processedClasses.add(clazz));
+        annotatedClasses.forEach(clazz -> {
+            if(registry.getInstance(clazz).isEmpty()) {
+                beanFactory.create(registry, annotatedClasses, clazz);
             }
-
-            if(processedClasses.isEmpty()) {
-                throw new CircularDependencyException();
-            }
-
-            annotatedClasses.removeAll(processedClasses);
-        } while(!annotatedClasses.isEmpty());
+        });
     }
 
     @SuppressWarnings("unchecked")
